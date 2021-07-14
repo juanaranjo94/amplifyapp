b0379f7a9c2b4c3dc9619385b69e716c23d83ff1
juan-s <juan.naranjo@dreamcodesoft.com>
2021-07-14 17:48:23 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esto es un prueba para construir un pagina en React y subirla a AWS
+          
         </p>
       </header>
     </div>
